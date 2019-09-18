58e13d64f37cfd3c85f74ee7c49044817a29b900
Eric O'Neill <oneill.eric23@gmail.com>
2019-09-18 10:29:48 -0600

    Add clearMessages action test to App mapDispatchToProps

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { App, mapStateToProps, mapDispatchToProps } from './App';
-import { removeUser, hasErrored } from '../../actions';
+import { removeUser, hasErrored, clearMessages } from '../../actions';
 import { endConversation } from '../../apiCalls';
 
 jest.mock('../../apiCalls');
@@ -104,6 +104,17 @@ describe('mapDispatchToProps', () => {
 
     const mappedProps = mapDispatchToProps(mockDispatch);
     mappedProps.hasErrored('fetch failed');
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  });
+
+  it('should call dispatch with a clearMessages action when clearMessages is called', () => {
+
+    let mockDispatch = jest.fn();
+    const actionToDispatch = clearMessages();
+
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.clearMessages();
 
     expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
   });
